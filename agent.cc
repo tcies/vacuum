a117ff347411c6a31aacfd9559992b07f64509e2
Henri Rebecq <h.rebecq@gmail.com>
2017-06-28 15:43:03 +0200

    Follow a simple homing strategy when the battery life falls below a threshold
    Change-Id: Ia839a38d9f32554b02bab942755f4e9efcdb90a9

--- agent.cc
+++ agent.cc
@@ -4,8 +4,12 @@
 
 #include "utils.h"
 
+#include <iostream>
+
 Agent::Agent(const Battery* battery, const int* time) :
-  battery_(battery), time_(time) {}
+  battery_(battery), time_(time) {
+    last_charging_signal_ = -1;  // uninitialized
+}
 
 Direction Agent::NextDirection(const Perception& p) {
   constexpr int strategy = 0;
@@ -13,9 +17,8 @@ Direction Agent::NextDirection(const Perception& p) {
   if (strategy == 0) {
     direction = randomStrategy(p);
   } else if (strategy == 1) {
-    
   }
-  
+
   Direction return_direction;
   if (returnHomeRequired(p, &return_direction)) {
     direction = return_direction;
@@ -40,8 +43,47 @@ Direction Agent::randomStrategy(const Perception& p) {
   return Direction::RIGHT;
 }
 
+Direction Agent::randomObstacleFreeDirection(const Perception& p) {
+    Direction direction;
+    bool found_obstacle_free_direction = false;
+    do {
+        direction = randomStrategy(p);
+        if (!p.HasObstacle(direction)) {
+            found_obstacle_free_direction = true;
+        }
+    } while (!found_obstacle_free_direction);
+
+    return direction;
+}
+
 bool Agent::returnHomeRequired(const Perception& p, Direction* direction) {
-  return false;
+    constexpr int min_battery_level = 50;
+    if (battery_->Level() < min_battery_level) {
+      std::cout << "Heading home!" << std::endl;
+
+      // Simple homing strategy:
+      //   * If the distance to the station has decreased
+      //     since the last step, go to a new obstacle free direction.
+      //   * If the distance has increased, just step back.
+      //
+      // If this is the first step, just go to a new obstacle free direction.
+
+      const float current_charging_signal = p.GetChargingSignal();
+      if (current_charging_signal > last_charging_signal_
+              && last_charging_signal_ >= 0) {
+          *direction = getOppositeDirection(last_direction_);
+      } else {
+          Direction possible_direction = randomObstacleFreeDirection(p);
+          *direction = possible_direction;
+          last_direction_ = possible_direction;
+      }
+
+      last_charging_signal_ = current_charging_signal;
+
+      return true;
+  } else {
+      return false;
+  }
 }
 
 bool Agent::IsAlive() const {
--- agent.h
+++ agent.h
@@ -15,11 +15,32 @@ class Agent {
 
   // Get the next direction of the agent based on the current perception |p|.
   Direction NextDirection(const Perception& p);
-  
+
   Direction randomStrategy(const Perception& p);
-  
+
+  static Direction getOppositeDirection(const Direction& direction) {
+      switch (direction) {
+          case Direction::LEFT:
+          default:
+              return Direction::RIGHT;
+          break;
+          case Direction::RIGHT:
+              return Direction::LEFT;
+          break;
+          case Direction::UP:
+              return Direction::DOWN;
+          break;
+          case Direction::DOWN:
+              return Direction::UP;
+          break;
+      }
+  }
+
+  // Finds a random direction that is obstacle free.
+  Direction randomObstacleFreeDirection(const Perception& p);
+
   // Returns true if it's required to return home, provides necessary direction
-  // to direction.
+  // to go back to the charging station.
   bool returnHomeRequired(const Perception& p, Direction* direction);
 
   // Whether the agent has still some battery left.
@@ -30,6 +51,10 @@ class Agent {
   const Battery* battery_;
   // Not owned by the agent.
   const int* time_;
+
+  // Variables needed by returnHomeRequired()
+  Direction last_direction_;
+  float last_charging_signal_;
 };
 
 #endif  // AGENT_H_
